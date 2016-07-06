d2a1c023b5e203b17843f4cf8951c6af68df9bc6
Anna Rogers <2muddybarefeet@gmail.com>
2016-07-05 18:13:53 -0700

    working on adding a history table

---
+++ RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by Anna Rogers on 7/5/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryViewController: UIViewController {
+    
+    var history = [RPSMatch]()
+    
+    override func viewDidLoad() {
+        print("view did load in history")
+    }
+    
+}
